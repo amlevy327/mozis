8d4234119c5677beae70c027de8544e18ca7436c
Andrew <amlevy327@gmail.com>
2021-11-20 08:44:38 -0600

    added token contract tests WIP

---
+++ Truffle/test/helpers.js
@@ -0,0 +1,7 @@
+/*
+export const EVM_REVERT = 'VM Exception while processing transaction: revert'
+
+export const URI_STRING = "www.me.com/"
+export const ROYALTY_ADDRESS = "0X00"
+export const ROYALTY_PERCENTAGE = 1000
+*/
